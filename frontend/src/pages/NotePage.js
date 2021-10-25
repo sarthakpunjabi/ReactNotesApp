e3531f567bcf8fab63de9d861e0a3f378ab8d5fe
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2021-10-25 01:56:40 +0100

    adding going back functionality to note

--- frontend/src/pages/NotePage.js
+++ frontend/src/pages/NotePage.js
@@ -1,5 +1,6 @@
 import React,{useState,useEffect} from 'react'
-
+import { ReactComponent as ArrowLeft } from '../assets/chevron-left.svg'
+import { Link } from 'react-router-dom'
 
 
 const NotePage = ({match}) => {
@@ -16,8 +17,15 @@ const NotePage = ({match}) => {
     }
 
     return (
-        <div>
-            <h1>{note?.body}</h1>
+        <div className="note">
+            <div className="note-header">
+                    <h3>
+                        <Link to="/">
+                            <ArrowLeft />
+                        </Link>
+                    </h3>
+            </div>
+            <textarea defaultValue={note?.body}></textarea>
         </div>
     )
 }
