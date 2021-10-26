6dbac8bfdd95f7436b53d7c6c2a23e15e7653e33
sarthakpunjabi <spunjabi2807.ca18@chitkara.edu.in>
2021-10-26 02:36:17 +0100

    adding right location of index.html

--- notespalace/settings.py
+++ notespalace/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'frontend/build'
+            BASE_DIR / 'build'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -129,6 +129,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'frontend/build/static'
 ]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
